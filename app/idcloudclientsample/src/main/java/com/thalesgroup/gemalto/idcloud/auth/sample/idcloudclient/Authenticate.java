80692b716bb8ee2c3f7963eecfadc64549a97f02
Himanshu Goel <himanshu.goel@thalesgroup.com>
2022-09-23 18:01:32 +0800

    Fixed Spacing

--- app/idcloudclientsample/src/main/java/com/thalesgroup/gemalto/idcloud/auth/sample/idcloudclient/Authenticate.java
+++ app/idcloudclientsample/src/main/java/com/thalesgroup/gemalto/idcloud/auth/sample/idcloudclient/Authenticate.java
@@ -33,18 +33,18 @@ public class Authenticate {
         // Set up an instance of UiCallbacks, an encapsulated class containing all necessary UI callbacks required by IdCloud FIDO SDK.
         // As a means of convenience, the IdCloud FIDO UI SDK provides a SampleSecurePinUiCallback,SampleCommonUiCallback class which conforms to the necessary callbacks of IdCloud FIDO SDK
         /* 1 */
-        ##Set up the necessary UI callbacks ##
+        ## Set up the necessary UI callbacks ##
 
         /* 2 */
-        ##Set fetch request callbacks ##
+        ## Set fetch request callbacks ##
 
         // Create an instance of the Fetch request.
         // Instances of requests should be held as an instance variable to ensure that callbacks will function as expected and to prevent unexpected behaviour.
         /* 3 */
-        ##Create a Fetch request callback ##
+        ## Create a Fetch request callback ##
 
         /* 4 */
-        //## Execute request ##
+        ## Execute request ##
 
     }
 }
--- app/idcloudclientsample/src/main/java/com/thalesgroup/gemalto/idcloud/auth/sample/idcloudclient/Enroll.java
+++ app/idcloudclientsample/src/main/java/com/thalesgroup/gemalto/idcloud/auth/sample/idcloudclient/Enroll.java
@@ -37,24 +37,24 @@ public class Enroll {
         // Set up an instance of UiCallbacks, an encapsulated class containing all necessary UI callbacks required by IdCloud FIDO SDK.
         // As a means of convenience, the IdCloud FIDO UI SDK provides a SampleSecurePinUiCallback,SampleCommonUiCallback class which conforms to the necessary callbacks of IdCloud FIDO SDK
         /* 1 */
-       ##Set up the necessary UI callbacks ##
+       ## Set up the necessary UI callbacks ##
 
         // Initialize an instance of EnrollmentToken from its corresponding Factory.
         // Instances of EnrollmentToken are initialized with a code retrieved from the Bank via a QR code (i.e. or other means) and is simply encoded as a UTF8 data.
         /* 2 */
-        ##Create an enrollment token ##
+        ## Create an enrollment token ##
 
         /* 3 */
-        ##Set enroll Request Call back ##
+        ## Set enroll Request Call back ##
 
 
         // Create an instance of the Enrollment request providing the required credentials.
         // Instances of requests should be held as an instance variable to ensure that completion callbacks will function as expected and to prevent unexpected behaviour.
         /* 4 */
-        ##Create an Enroll request ##
+        ## Create an Enroll request ##
 
         /* 5 */
-        ##Execute the request ##
+        ## Execute the request ##
 
     }
 
